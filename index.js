aeb7c4009cfb81b50efe328d041c0c427c5bba25
Alexia <96691787+kitsunebunny@users.noreply.github.com>
2024-02-27 10:39:54 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+client.on(Events.InteractionCreate, async interaction => {
+	if (interaction.isChatInputCommand()) {
+		// command handling
+	} else if (interaction.isAutocomplete()) {
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.autocomplete(interaction);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+});
