7857ee7942746ede1ee89768a0b8e0c2c37037b4
Alexia <96691787+kitsunebunny@users.noreply.github.com>
2024-02-27 14:08:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const { ShardingManager } = require('discord.js');
 
-const manager = new ShardingManager('./bot.js', { token: 'your-token-goes-here' });
+const manager = new ShardingManager('./bot.js', { token: 'MTIwNTUxMzcyMDI3NzYzOTIxOQ.GpGqo2.fqnY4I11AoPN8_5dnzksVi0J1bfzIg9qH-u4II
+ });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
