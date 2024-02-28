98ce7ddc1795274c4c6a524ccee34359b9d0961b
Alexia <96691787+kitsunebunny@users.noreply.github.com>
2024-02-28 14:16:11 +0200

    Create dbObjects.js

---
+++ dbObjects.js
@@ -0,0 +1,21 @@
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const currency = new Collection();
+
+async function addBalance(id, amount) {
+	const user = currency.get(id);
+
+	if (user) {
+		user.balance += Number(amount);
+		return user.save();
+	}
+
+	const newUser = await Users.create({ user_id: id, balance: amount });
+	currency.set(id, newUser);
+
+	return newUser;
+}
+
+function getBalance(id) {
+	const user = currency.get(id);
+	return user ? user.balance : 0;
+}
