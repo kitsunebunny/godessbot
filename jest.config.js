5e131a2f6aac87137350f4187f66fab6b79d4e3c
Alexia <96691787+kitsunebunny@users.noreply.github.com>
2024-02-29 13:13:37 +0200

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/[^/]*)\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
