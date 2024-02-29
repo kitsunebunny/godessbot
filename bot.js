51110e29ea048488d21354ff91788ad591451727
Alexia <96691787+kitsunebunny@users.noreply.github.com>
2024-02-29 12:27:22 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const { ShardingManager } = require('discord.js');
-
-const manager = new ShardingManager('./bot.js', { token: 'MTIwNTUxMzcyMDI3NzYzOTIxOQ.GpGqo2.fqnY4I11AoPN8_5dnzksVi0J1bfzIg9qH-u4II
- });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
-manager.spawn();
